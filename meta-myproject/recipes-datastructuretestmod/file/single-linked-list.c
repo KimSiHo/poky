8f9231b6595cec1b0dbcdc7126c35feb6c0f19a8
siho.kim <odreamgodo@gmail.com>
2025-01-01 21:43:06 +0900

    datastructure test: modify single linked list test

--- meta-myproject/recipes-datastructuretestmod/file/single-linked-list.c
+++ meta-myproject/recipes-datastructuretestmod/file/single-linked-list.c
@@ -12,21 +12,45 @@ static struct hlist_head my_list = HLIST_HEAD_INIT;
 
 int single_linked_list_test(void)
 {
-    struct my_struct *item = kmalloc(sizeof(struct my_struct), GFP_KERNEL);
-    if (!item)
-        return -ENOMEM;
-    item->data = 42;
-    hlist_add_head(&item->node, &my_list);
+    struct my_struct *item1, *item2, *item3;
 
+    item1 = kmalloc(sizeof(struct my_struct), GFP_KERNEL);
+    if (!item1)
+        return -ENOMEM;
+    item1->data = 42;
+    hlist_add_head(&item1->node, &my_list);
+
+    item2 = kmalloc(sizeof(struct my_struct), GFP_KERNEL);
+    if (!item2) {
+        kfree(item1);
+        return -ENOMEM;
+    }
+    item2->data = 24;
+    hlist_add_head(&item2->node, &my_list);
+
+    item3 = kmalloc(sizeof(struct my_struct), GFP_KERNEL);
+    if (!item3) {
+        kfree(item1);
+        kfree(item2);
+        return -ENOMEM;
+    }
+    item3->data = 84;
+    hlist_add_head(&item3->node, &my_list);
+
+    // 리스트 순회
     struct hlist_node *pos;
+    pr_info("Traversing the single-linked list\n");
     hlist_for_each(pos, &my_list) {
         struct my_struct *entry = hlist_entry(pos, struct my_struct, node);
-        pr_info("Data: %d \n", entry->data);
+        pr_info("Data: %d\n", entry->data);
     }
 
+    // 리스트 정리 및 메모리 해제
     struct hlist_node *pos_safe, *tmp;
+    pr_info("Deleting nodes and freeing memory\n");
     hlist_for_each_safe(pos_safe, tmp, &my_list) {
         struct my_struct *entry = hlist_entry(pos_safe, struct my_struct, node);
+        pr_info("Freeing node with data: %d\n", entry->data);
         hlist_del(&entry->node);
         kfree(entry);
     }
